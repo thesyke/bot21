254af0fe722174c25abac1e5e1f081c0dc6f2a82
thesyke <111369892+thesyke@users.noreply.github.com>
2026-05-19 00:52:09 +0300

    Update config.js

--- config.js
+++ config.js
@@ -62,7 +62,8 @@ export const CATALOG = {
     products: {
        wd: { label: "verde", prices: { 5: 50, 10: 90 } },
        cris: { label: "cris", prices: { 1: 35, 2: 70 } }
-    },
+    }
+      },
          
       cluj: {
     label: "Cluj",
